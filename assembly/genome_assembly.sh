89a96e2f4626abd848992a3863fe2c70de652978
Alina Orozco <alina.orozco12@gmail.com>
2019-04-04 06:40:46 -0700

    Canu assembly

---
+++ assembly/genome_assembly.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -L
+
+#SBATCH -A g2019003
+#SBATCH -p core
+#SBATCH -n 2
+#SBATCH 05:30:00
+#SBATCH -J assembly
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user alina.orozco12@gmail.com
+
+module load bioinfo-tools
+module load canu
+
+canu -p canu -d canu_outdir genomeSize=2.7m -pacbio-raw /home/alina0/Paper1_raw_data/genomics_data/PacBio/*
+
