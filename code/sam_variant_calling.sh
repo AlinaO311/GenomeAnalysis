dd86ce0603b606c38daa730a490acb89efe407ea
AlinaO311 <alina.orozco12@gmail.com>
2019-04-11 15:12:45 +0200

    pushing .sh files

--- code/sam_variant_calling.sh
+++ code/sam_variant_calling.sh
@@ -1,15 +1,17 @@
 #!/bin/bash -l
-
 #SBATCH -A g2019003
 #SBATCH -p core
 #SBATCH -n 2
 #SBATCH -t 02:00:00
-#SBATCH -J sam_canu_coverage
+#SBATCH -J sam_variant
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user alina.orozco12@gmail.com
 
 module load bioinfo-tools
 module load samtools
 
-samtools depth canu_illumina_aln.bam > canu_illumina.coverage
+samtools faidx canu.contigs.fasta 
 
+samtools sort canu_illumina_aln.bam canu_illumina_sorted.bam
+
+samtools index canu_illumina_sorted.bam
