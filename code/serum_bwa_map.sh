6a75dc095ce15931c5e2b4a64e28bd8aead5990f
AlinaO311 <alina.orozco12@gmail.com>
2019-05-16 15:34:42 +0200

    Working serum map

---
+++ code/serum_bwa_map.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -l
+#SBATCH -A g2019003
+#SBATCH -p core
+#SBATCH -n 2
+#SBATCH -t 05:00:00
+#SBATCH -J pilon_serum_aln
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user alina.orozco12@gmail.com
+
+module load bioinfo-tools
+module load bwa
+module load samtools
+
+bwa index -p rna_bh_map /home/alina0/analysis/transcriptome/alignment/BH/pilon2.fasta
+bwa mem -M rna_bh_map trim_paired_ERR1797969_pass_1.fastq.gz trim_paired_ERR1797969_pass_2.fastq.gz | samtools sort -o rna_bh_map69.bam
+
+module load bioinfo-tools
+module load bwa
+module load samtools
+
+bwa index -p rna_bh_map /home/alina0/analysis/transcriptome/alignment/BH/pilon2.fasta
+bwa mem -M rna_bh_map trim_paired_ERR1797970_pass_1.fastq.gz trim_paired_ERR1797970_pass_2.fastq.gz | samtools sort -o rna_bh_map70.bam
+
+module load bioinfo-tools
+module load bwa
+module load samtools
+
+bwa index -p rna_bh_map /home/alina0/analysis/transcriptome/alignment/BH/pilon2.fasta
+bwa mem -M rna_bh_map trim_paired_ERR1797971_pass_1.fastq.gz trim_paired_ERR1797971_pass_2.fastq.gz | samtools sort -o rna_bh_map71.bam
+
+
+
+
+
