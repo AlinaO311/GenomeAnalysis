a6e1ded1a6a5e79e8363a9960f622c01a1e82a87
Alina Orozco <alina.orozco12@gmail.com>
2019-05-17 06:06:47 -0700

    Update pilon_assembly.sh

--- code/pilon_assembly.sh
+++ code/pilon_assembly.sh
@@ -6,7 +6,7 @@
 #SBATCH -t 04:00:00
 #SBATCH -J pilon
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user alina.orozco12@gmail.com
+#SBATCH --mail-user email
 
 module load bioinfo-tools
 module load Pilon
