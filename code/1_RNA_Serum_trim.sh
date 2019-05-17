404c31bafdd00b35a72c27aa5d28cd5c5a11303e
Alina Orozco <alina.orozco12@gmail.com>
2019-05-17 06:02:29 -0700

    Update 1_RNA_Serum_trim.sh

--- code/1_RNA_Serum_trim.sh
+++ code/1_RNA_Serum_trim.sh
@@ -6,7 +6,7 @@
 #SBATCH -t 04:00:00
 #SBATCH -J rna_trimming
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user alina.orozco12@gmail.com
+#SBATCH --mail-user email
 
 module load bioinfo-tools
 module load trimmomatic
